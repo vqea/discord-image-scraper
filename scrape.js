2911a107dc50b650501695e43e1025c563c481d2
emily <110758135+vqea@users.noreply.github.com>
2023-03-18 12:53:33 -0400

    Create scrape.js

---
+++ scrape.js
@@ -0,0 +1,57 @@
+const { token, fetchChanel_id } = require("./settings.json");
+
+const { greenBright } = require("chalk");
+const ora = require("ora");
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+
+Main();
+async function request(before) {
+    const options = {
+        method: "GET",
+        headers: { authorization: token }
+    };
+    const spinner = ora("Sending 'GET' Request(s)").start();
+    setTimeout(() => {
+        spinner.stop();
+    }, 1000);
+    const request = await fetch(
+        `https://discord.com/api/channels/${fetchChanel_id}/messages?limit=100&${before ? "before=" + before : ""}`,
+        options
+    );
+    return await request.json();
+}
+
+let result;
+
+async function go() {
+    let page = await request();
+    result = page;
+
+    while (page.length >= 100) {
+        page = await request(page[page.length - 1].id);
+        result = result.concat(page);
+    }
+    const spinner = ora("Fetching...").start();
+    setTimeout(() => {
+        let linkArr = [];
+        spinner.succeed(greenBright(`Fetched ${result.length} messages`));
+        const content = JSON.stringify(result, null, 2);
+        fs.writeFileSync("links.json", content);
+        const links = require("./links.json");
+        const scraped = links.map((attach) => attach.attachments.map(url => url.proxy_url)).map((u) => u).map(u => u).map((getlink) => getlink.toString()).filter(e => e);
+        const orgArr = scraped.filter((link) => link.includes(",") === false);
+        const fil = scraped.filter((link) => link.includes(","));
+        const newArr = fil.map(l =>  l.split(',') ).flat();
+        const finalArr = linkArr.concat(orgArr, newArr);
+        const content2 = JSON.stringify(finalArr, null, 2);
+        spinner.succeed(greenBright(`Extracted ${finalArr.length} images | Check "links.json"`));
+        fs.writeFileSync("links.json", content2);
+        process.exit(1);
+    }, 2000);
+}
+
+function Main() {
+    request().then(() => { go(); });
+}
